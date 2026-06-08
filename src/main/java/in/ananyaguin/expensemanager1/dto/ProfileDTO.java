91dd0f84020074419561897b31c174b1b3931e38
Ananya Guin <ananyaguin96@gmail.com>
2026-06-08 17:36:45 +0530

    Chanhge DTO Package

--- src/main/java/in/ananyaguin/expensemanager1/dto/ProfileDTO.java
+++ src/main/java/in/ananyaguin/expensemanager1/dto/ProfileDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ProfileDto {
+public class ProfileDTO {
 
     private Long id;
     private String fullName;
