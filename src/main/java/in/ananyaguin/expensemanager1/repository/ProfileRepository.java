35df23cf577857a8a98d0aa3f0e46e9ad78766c8
Ananya Guin <ananyaguin96@gmail.com>
2026-06-29 19:40:24 +0530

    Activation Profile

--- src/main/java/in/ananyaguin/expensemanager1/repository/ProfileRepository.java
+++ src/main/java/in/ananyaguin/expensemanager1/repository/ProfileRepository.java
@@ -11,4 +11,5 @@ public interface ProfileRepository extends JpaRepository <ProfileEntity,Long>{
     Optional<ProfileEntity> findByEmail(String email);
     Optional<ProfileEntity> findByActivationToken(String activationToken);
 
+
 }
