924e3bc3d3e895009c93e77f825a21db6d529dd3
Ananya Guin <ananyaguin96@gmail.com>
2026-08-22 23:06:18 +0530

    Change Excel sheet

--- src/main/java/in/ananyaguin/expensemanager1/service/ExcelService.java
+++ src/main/java/in/ananyaguin/expensemanager1/service/ExcelService.java
@@ -16,6 +16,7 @@ import java.util.stream.IntStream;
 @Service
 public class ExcelService {
 
+
     public void writeIncomesToExcel(
             OutputStream os,
             List<IncomeDTO> incomes) throws IOException {
@@ -24,6 +25,7 @@ public class ExcelService {
 
             Sheet sheet = workbook.createSheet("Incomes");
 
+            // Header
             Row header = sheet.createRow(0);
 
             header.createCell(0).setCellValue("S.No");
@@ -32,6 +34,7 @@ public class ExcelService {
             header.createCell(3).setCellValue("Amount");
             header.createCell(4).setCellValue("Date");
 
+            // Data
             IntStream.range(0, incomes.size())
                     .forEach(i -> {
 
@@ -40,38 +43,42 @@ public class ExcelService {
                         Row row = sheet.createRow(i + 1);
 
                         row.createCell(0).setCellValue(i + 1);
-                        row.createCell(1)
-                                .setCellValue(
-                                        income.getName() != null
-                                                ? income.getName()
-                                                : "N/A"
-                                );
 
-                        row.createCell(2)
-                                .setCellValue(
-                                        income.getCategoryId() != null
-                                                ? income.getCategoryName()
-                                                : "N/A"
-                                );
+                        row.createCell(1).setCellValue(
+                                income.getName() != null
+                                        ? income.getName()
+                                        : "N/A"
+                        );
 
-                        row.createCell(3)
-                                .setCellValue(
-                                        income.getAmount() != null
-                                                ? income.getAmount().doubleValue()
-                                                : 0
-                                );
+                        row.createCell(2).setCellValue(
+                                income.getCategoryName() != null
+                                        ? income.getCategoryName()
+                                        : "N/A"
+                        );
 
-                        row.createCell(4)
-                                .setCellValue(
-                                        income.getDate() != null
-                                                ? income.getDate().toString()
-                                                : "N/A"
-                                );
+                        row.createCell(3).setCellValue(
+                                income.getAmount() != null
+                                        ? income.getAmount().doubleValue()
+                                        : 0
+                        );
+
+                        row.createCell(4).setCellValue(
+                                income.getDate() != null
+                                        ? income.getDate().toString()
+                                        : "N/A"
+                        );
                     });
+
+            // Adjust column width
+            for (int i = 0; i < 5; i++) {
+                sheet.autoSizeColumn(i);
+            }
 
             workbook.write(os);
         }
     }
+
+
 
     public void writeExpensesToExcel(
             OutputStream os,
@@ -81,6 +88,7 @@ public class ExcelService {
 
             Sheet sheet = workbook.createSheet("Expenses");
 
+            // Header
             Row header = sheet.createRow(0);
 
             header.createCell(0).setCellValue("S.No");
@@ -89,6 +97,7 @@ public class ExcelService {
             header.createCell(3).setCellValue("Amount");
             header.createCell(4).setCellValue("Date");
 
+            // Data
             IntStream.range(0, expenses.size())
                     .forEach(i -> {
 
@@ -98,34 +107,36 @@ public class ExcelService {
 
                         row.createCell(0).setCellValue(i + 1);
 
-                        row.createCell(1)
-                                .setCellValue(
-                                        expense.getName() != null
-                                                ? expense.getName()
-                                                : ""
-                                );
+                        row.createCell(1).setCellValue(
+                                expense.getName() != null
+                                        ? expense.getName()
+                                        : "N/A"
+                        );
 
-                        row.createCell(2)
-                                .setCellValue(
-                                        expense.getCategoryId() != null
-                                                ? expense.getCategoryName()
-                                                : "N/A"
-                                );
+                        row.createCell(2).setCellValue(
+                                expense.getCategoryName() != null
+                                        ? expense.getCategoryName()
+                                        : "N/A"
+                        );
 
-                        row.createCell(3)
-                                .setCellValue(
-                                        expense.getAmount() != null
-                                                ? expense.getAmount().doubleValue()
-                                                : 0
-                                );
+                        row.createCell(3).setCellValue(
+                                expense.getAmount() != null
+                                        ? expense.getAmount().doubleValue()
+                                        : 0
+                        );
 
-                        row.createCell(4)
-                                .setCellValue(
-                                        expense.getDate() != null
-                                                ? expense.getDate().toString()
-                                                : ""
-                                );
+                        row.createCell(4).setCellValue(
+                                expense.getDate() != null
+                                        ? expense.getDate().toString()
+                                        : "N/A"
+                        );
                     });
+
+            // Adjust column width
+            for (int i = 0; i < 5; i++) {
+                sheet.autoSizeColumn(i);
+            }
+
             workbook.write(os);
         }
     }
